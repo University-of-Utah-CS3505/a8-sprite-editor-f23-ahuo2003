123002289d3189a97a6aeb708f2b0de3becfef0a
Thomas <thomaskuhrke24@gmail.com>
2023-11-07 14:48:39 -0700

    Created Frame and Tool headers. Finished the Model.h header.

---
+++ Frame.h
@@ -0,0 +1,24 @@
+#ifndef FRAME_H
+#define FRAME_H
+
+class Frame : public QObject {
+    Q_OBJECT
+public:
+    /**
+     * @brief Tool constructor
+     * @param parent a QObject
+     */
+    explicit Frame(QObject *parent = nullptr);
+
+
+public slots:
+    // TODO
+
+signals:
+    // TODO
+
+private:
+    //TODO
+};
+
+#endif // FRAME_H
---
+++ Tool.h
@@ -0,0 +1,23 @@
+#ifndef TOOL_H
+#define TOOL_H
+
+class Tool : public QObject {
+    Q_OBJECT
+public:
+    /**
+     * @brief Tool constructor
+     * @param parent a QObject
+     */
+    explicit Tool(QObject *parent = nullptr);
+
+
+public slots:
+    // TODO
+
+signals:
+    // TODO
+
+private:
+    //TODO
+};
+#endif // TOOL_H
