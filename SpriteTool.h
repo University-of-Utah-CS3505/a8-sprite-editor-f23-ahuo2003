0dd8ca713f818c6f24414512184ccdeba3f5f8fa
RaZe <razerizer@gmail.com>
2023-11-12 19:52:47 -0700

    Added friend class SpriteModel so it could access protected methods.

--- SpriteTool.h
+++ SpriteTool.h
@@ -2,11 +2,15 @@
 #define SPRITETOOL_H
 
 class SpriteTool {
-    public:
-        SpriteTool();
-        virtual void mousePressed();
-        virtual void mouseReleased();
-        virtual void mouseMoved();
+public:
+  SpriteTool();
+
+protected:
+  virtual void mousePressed();
+  virtual void mouseReleased();
+  virtual void mouseMoved();
+
+  friend class SpriteModel;
 };
 
 #endif // SPRITETOOL_H
