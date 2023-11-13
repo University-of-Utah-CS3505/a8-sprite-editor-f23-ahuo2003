ddc36298cbe8096a9af09f21743f6d6ffdb86add
RaZe <razerizer@gmail.com>
2023-11-12 19:25:45 -0700

    added Cursor class to be used as default tool.

---
+++ cursor.h
@@ -0,0 +1,16 @@
+#ifndef CURSOR_H
+#define CURSOR_H
+
+#include "SpriteTool.h"
+
+class Cursor : public SpriteTool {
+public:
+  Cursor();
+
+protected:
+  virtual void mousePressed();
+  virtual void mouseReleased();
+  virtual void mouseMoved();
+};
+
+#endif // CURSOR_H
