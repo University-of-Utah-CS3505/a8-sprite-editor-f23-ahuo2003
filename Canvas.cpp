8bb915cae45f46f86114a40a4d78d1279c789377
Thomas <thomaskuhrke24@gmail.com>
2023-11-13 13:24:20 -0700

    Deleted Mouse Action comments.

--- Canvas.cpp
+++ Canvas.cpp
@@ -12,13 +12,10 @@ void Canvas::redrawCanvas(QImage frame)
 
 void Canvas::mousePressEvent(QMouseEvent *event) {
   emit mouseEventSignal(event);
-  std::cout << "MousePressed" << std::endl;
 }
 void Canvas::mouseReleaseEvent(QMouseEvent *event) {
   emit mouseEventSignal(event);
-  std::cout << "mouseReleased" << std::endl;
 }
 void Canvas::mouseMoveEvent(QMouseEvent *event) {
   emit mouseEventSignal(event);
-  std::cout << "MouseMoved" << std::endl;
 }
