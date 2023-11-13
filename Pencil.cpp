ab5588d0d8a969c414e51c6eed9f5860ca8589bc
Thomas <thomaskuhrke24@gmail.com>
2023-11-12 21:42:55 -0700

    Provided possible implementation for the Pencil, and adapted .h method with new parameters

--- Pencil.cpp
+++ Pencil.cpp
@@ -1,13 +1,44 @@
 #include "Pencil.h"
+#include <QMouseEvent>
+
 
 Pencil::Pencil() : SpriteTool(){}
 
-void Pencil::mousePressed(){
-
+void Pencil::drawPixel(QImage& image, QColor& currColor, QMouseEvent *event){
+    image.setPixelColor(event->pos(), currColor);
 }
+
+void Pencil::mousePressed(QImage& image, QColor& currColor, QMouseEvent *event) {
+    drawPixel(image, currColor, event);
+}
+
 void Pencil::mouseReleased(){
 
 }
 void Pencil::mouseMoved(){
+//    //Draw based on current cord and previous
+//    //x1 and y1
+//    startX = event->pos().x();
+//    startY = event->pos().y();
 
+//    //x2 and y2
+//    endX = event->pos().x();
+//    endY = event->pos().y();
+
+//    //Horizonal line or Regular Line
+//    if (endX != startX){
+//        int slope = (endY-startY)/(endX-startX);
+//        for (int i = startX; i < endX; i++){
+//            endY = slope * (endX-startY);
+//            QPoint newPoint(endX,endY);
+//            drawPixel(image, newPoint, currColor);
+//        }
+//    }
+//    //Vertical Line
+//    else if (endY != startY){
+//        while (startY < endY) {
+//            QPoint newPoint(startX, startY + 1);
+//            drawPixel(image, newPoint, currColor);
+//        }
+//    }
 }
--- Pencil.h
+++ Pencil.h
@@ -1,13 +1,17 @@
 #ifndef PENCIL_H
 #define PENCIL_H
 #include "SpriteTool.h"
+#include <QImage>
+#include <QColor>
+#include <QMouseEvent>
 
 class Pencil : public SpriteTool {
 public:
   Pencil();
+  void drawPixel(QImage& image, QColor& currColor, QMouseEvent *event);
 
 protected:
-  virtual void mousePressed();
+  virtual void mousePressed(QImage& image, QColor& currColor, QMouseEvent *event);
   virtual void mouseReleased();
   virtual void mouseMoved();
 };
