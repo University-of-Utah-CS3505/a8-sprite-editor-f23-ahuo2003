4fe75a8605ec2dddce81d08991c51c65d3c2ad13
RaZe <razerizer@gmail.com>
2023-11-12 19:51:18 -0700

    Added Canvas object for custom mouse event handling.

--- Canvas.cpp
+++ Canvas.cpp
@@ -2,14 +2,17 @@
 #include <iostream>
 
 // General Tool Declaration
-Canvas::Canvas(QLabel *parent) : QLabel(parent){}
+Canvas::Canvas(QWidget *parent) : QLabel(parent) {}
 
-void Canvas::mousePressEvent(QMouseEvent *event){
-    std::cout << "MousePressed" << std::endl;
+void Canvas::mousePressEvent(QMouseEvent *event) {
+  emit mouseEventSignal(event);
+  std::cout << "MousePressed" << std::endl;
 }
-void Canvas::mouseReleaseEvent(QMouseEvent *event){
-    std::cout << "mouseReleased" << std::endl;
+void Canvas::mouseReleaseEvent(QMouseEvent *event) {
+  emit mouseEventSignal(event);
+  std::cout << "mouseReleased" << std::endl;
 }
-void Canvas::mouseMoveEvent(QMouseEvent *event){
-    std::cout << "MouseMoved" << std::endl;
+void Canvas::mouseMoveEvent(QMouseEvent *event) {
+  emit mouseEventSignal(event);
+  std::cout << "MouseMoved" << std::endl;
 }
--- Canvas.h
+++ Canvas.h
@@ -1,16 +1,20 @@
 #ifndef CANVAS_H
 #define CANVAS_H
-#include <QMouseEvent>
 #include <QLabel>
+#include <QMouseEvent>
 
 class Canvas : public QLabel {
-    Q_OBJECT
-    public:
-        Canvas(QLabel *parent = nullptr);
-    protected:
-        virtual void mousePressEvent(QMouseEvent *event) override;
-        virtual void mouseReleaseEvent(QMouseEvent *event) override;
-        virtual void mouseMoveEvent(QMouseEvent *event) override;
+  Q_OBJECT
+public:
+  Canvas(QWidget *parent = nullptr);
+
+signals:
+  void mouseEventSignal(QMouseEvent *event);
+
+protected:
+  virtual void mousePressEvent(QMouseEvent *event) override;
+  virtual void mouseReleaseEvent(QMouseEvent *event) override;
+  virtual void mouseMoveEvent(QMouseEvent *event) override;
 };
 
 #endif // CANVAS_H
