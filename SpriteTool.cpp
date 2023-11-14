9ceaf50f19ea144c29d140b87fd68b24f7f4bbb5
Thomas <thomaskuhrke24@gmail.com>
2023-11-14 01:48:28 -0700

    Added protected name variable, as well as the scaling factor to the virtual method for pixel location calculations.

--- SpriteTool.cpp
+++ SpriteTool.cpp
@@ -3,16 +3,15 @@
 // General Tool Declaration
 SpriteTool::SpriteTool(){}
 
-void SpriteTool::mousePressed(QImage& image, QColor& currColor, QMouseEvent *event){
+void SpriteTool::mousePressed(QImage& image, QColor& currColor, QMouseEvent *event, int scaleFactor){}
+void SpriteTool::mouseReleased(){}
+void SpriteTool::mouseMoved(QImage& image, QColor& currColor, QMouseEvent *event, int scaleFactor){}
 
-}
-void SpriteTool::mouseReleased(){
-
-}
-void SpriteTool::mouseMoved(QImage& image, QColor& currColor, QMouseEvent *event){
-
-}
-
-QPoint SpriteTool::mapToImageCoordinates(const QPoint &point) {
+QPoint SpriteTool::mapToImageCoordinates(const QPoint &point, int scaleFactor) {
     return QPoint(point.x() / scaleFactor, point.y() / scaleFactor);
+}
+
+QString SpriteTool::getName() const
+{
+    return name;
 }
--- SpriteTool.h
+++ SpriteTool.h
@@ -3,22 +3,22 @@
 #include <QImage>
 #include <QColor>
 #include <QMouseEvent>
+#include <QString>
 
 class SpriteTool {
 public:
   SpriteTool();
+  QString getName() const;
 
 protected:
   virtual void mousePressed(QImage &image, QColor &currColor,
-                            QMouseEvent *event);
+                            QMouseEvent *event, int scaleFactor);
   virtual void mouseReleased();
-  virtual void mouseMoved(QImage &image, QColor &currColor, QMouseEvent *event);
-  QPoint mapToImageCoordinates(const QPoint &point);
+  virtual void mouseMoved(QImage &image, QColor &currColor, QMouseEvent *event, int scaleFactor);
+  QPoint mapToImageCoordinates(const QPoint &point, int scaleFactor);
+  QString name;
 
   friend class SpriteModel;
-
-private:
-  int scaleFactor;
 };
 
 #endif // SPRITETOOL_H
