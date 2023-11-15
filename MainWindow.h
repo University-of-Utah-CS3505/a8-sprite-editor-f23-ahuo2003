d63f47f689eea803494ecd7cc1d05be8008e9f9f
ahuo2003 <tigerandy@hotmail.com>
2023-11-15 13:58:59 -0700

    Added comments to MainWIndow.h and ScaleCanvas.h

--- MainWindow.h
+++ MainWindow.h
@@ -19,33 +19,127 @@ public:
     MainWindow(SpriteModel& model, Canvas& canvas, QWidget *parent = nullptr);
     ~MainWindow();
 
+    /**
+     * @brief SetIcons sets the various icons in our buttons
+     */
     void SetIcons();
 
 public slots:
+    /**
+     * @brief onSlidersValueChanged handles the event when one of the RGB sliders is changed
+     */
     void onSlidersValueChanged();
+
+    /**
+     * @brief changeColorPreview changes the current color being displayed in our preview box
+     * @param red the red value in RGB
+     * @param green the green value in RGB
+     * @param blue the blue value in RGB
+     */
     void changeColorPreview(int red, int green, int blue);
+
+    /**
+     * @brief cursorToggled handles the event when the cursor tool is selected
+     */
     void cursorToggled();
+
+    /**
+     * @brief pencilToggled handles the event when the pencil tool is selected
+     */
     void pencilToggled();
+
+    /**
+     * @brief eraserToggled handles the event when the eraser tool is selected
+     */
     void eraserToggled();
+
+    /**
+     * @brief bucketToggled handles the event when the bucket tool is selected
+     */
     void bucketToggled();
+
+    /**
+     * @brief eyeDropperToggled handles the event when the eydropper tool is selected
+     */
     void eyeDropperToggled();
+
+    /**
+     * @brief changeSlidersColor handles the event for changing the color sliders
+     * @param color the color to be changed to
+     */
     void changeSlidersColor(QColor color);
+
+    /**
+     * @brief setCanvasSize change the size of the drawing canvas
+     */
     void setCanvasSize();
+
+    /**
+     * @brief four change the size of the canvas to four
+     */
     void four();
+
+    /**
+     * @brief eight change the size of the canvas to eight
+     */
     void eight();
+    /**
+     * @brief sixteen change the size of the canvas to sixteen
+     */
     void sixteen();
+
+    /**
+     * @brief thirtyTwo change the size of the canvas to thirtytwo
+     */
     void thirtyTwo();
+
+    /**
+     * @brief sixtyFour change the size of the canvas to sixtyfour
+     */
     void sixtyFour();
 signals:
+    /**
+     * @brief changeModelCurrentColor change the current selected color
+     * @param red red value
+     * @param green green value
+     * @param blue blue value
+     */
     void changeModelCurrentColor(int red, int green, int blue);
+
+    /**
+     * @brief changeTool change the current selected tool
+     * @param toolName the tool to be changed to
+     */
     void changeTool(QString toolName);
+
+    /**
+     * @brief getSize send the size of the new canvas
+     * @param size the new size
+     */
     void getSize(QSize size);
 
 private:
+    // The UI for this MainWindow
     Ui::MainWindow *ui;
+
+    /**
+     * @brief loadStyleSheet set the StyleSheet
+     */
     void loadStyleSheet();
+
+    /**
+     * @brief initCanvas set the drawing canvas
+     */
     void initCanvas();
+
+    /**
+     * @brief initPreviews set the preview canvas
+     */
     void initPreviews();
+
+    /**
+     * @brief initSliders set the sliders
+     */
     void initSliders();
 };
 #endif // MAINWINDOW_H
--- ScaleCanvas.h
+++ ScaleCanvas.h
@@ -15,13 +15,33 @@ public:
     explicit ScaleCanvas(QWidget *parent = nullptr);
     ~ScaleCanvas();
 public slots:
+    /**
+     * @brief fourSelected set the selected canvas size to four
+     */
     void fourSelected();
+
+    /**
+     * @brief eightSelected set the selected canvas size to eight
+     */
     void eightSelected();
+
+    /**
+     * @brief sixteenSelected set the selected canvas size to sixteen
+     */
     void sixteenSelected();
+
+    /**
+     * @brief thirtytwoSelected set the selected canvas size to thirty two
+     */
     void thirtytwoSelected();
+
+    /**
+     * @brief sixtyfourSelected set the selected canvas size to sixty four
+     */
     void sixtyfourSelected();
 
 private:
+    // The UI for this window
     Ui::ScaleCanvas *ui;
 };
 
