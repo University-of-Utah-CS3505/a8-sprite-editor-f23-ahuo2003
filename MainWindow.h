e8b4b5824169dd7952d74dfa0e0311e07f09eeb3
RaZe <razerizer@gmail.com>
2023-11-16 00:22:42 -0700

    Added comments

--- MainWindow.h
+++ MainWindow.h
@@ -13,187 +13,192 @@ class MainWindow;
 QT_END_NAMESPACE
 
 class MainWindow : public QMainWindow {
-    Q_OBJECT
+  Q_OBJECT
 
 public:
-    MainWindow(SpriteModel &model, Canvas &canvas, QWidget *parent = nullptr);
-    ~MainWindow();
-
-    /**
-   * @brief SetIcons sets the various icons in our buttons
-   */
-    void SetIcons();
-
-    void SetFileTab();
-
-    void initSizeButtons();
+  MainWindow(SpriteModel &model, Canvas &canvas, QWidget *parent = nullptr);
+  ~MainWindow();
 
 public slots:
-    /**
+  /**
    * @brief onSlidersValueChanged handles the event when one of the RGB sliders
    * is changed
    */
-    void onSlidersValueChanged();
+  void onSlidersValueChanged();
 
-    /**
+  /**
    * @brief changeColorPreview changes the current color being displayed in our
    * preview box
    * @param red the red value in RGB
    * @param green the green value in RGB
    * @param blue the blue value in RGB
    */
-    void changeColorPreview(int red, int green, int blue);
+  void changeColorPreview(int red, int green, int blue);
 
-    /**
+  /**
    * @brief cursorToggled handles the event when the cursor tool is selected
    */
-    void cursorToggled();
+  void cursorToggled();
 
-    /**
+  /**
    * @brief pencilToggled handles the event when the pencil tool is selected
    */
-    void pencilToggled();
+  void pencilToggled();
 
-    /**
+  /**
    * @brief eraserToggled handles the event when the eraser tool is selected
    */
-    void eraserToggled();
+  void eraserToggled();
 
-    /**
+  /**
    * @brief bucketToggled handles the event when the bucket tool is selected
    */
-    void bucketToggled();
+  void bucketToggled();
 
-    /**
+  /**
    * @brief eyeDropperToggled handles the event when the eydropper tool is
    * selected
    */
-    void eyeDropperToggled();
+  void eyeDropperToggled();
 
-    /**
-     * @brief saveToggled TODO
-     */
-    void saveToggled();
+  /**
+   * @brief saveToggled TODO
+   */
+  void saveToggled();
 
-    /**
-     * @brief loadToggled TODO
-     */
-    void loadToggled();
+  /**
+   * @brief loadToggled TODO
+   */
+  void loadToggled();
 
-    /**
-     * @brief disablePreviousButton TODO
-     */
-    void disablePreviousButton();
+  /**
+   * @brief disablePreviousButton TODO
+   */
+  void disablePreviousButton();
 
-    /**
-     * @brief disableNextButton TODO
-     */
-    void disableNextButton();
+  /**
+   * @brief disableNextButton TODO
+   */
+  void disableNextButton();
 
-    /**
+  /**
    * @brief changeSlidersColor handles the event for changing the color sliders
    * @param color the color to be changed to
    */
-    void changeSlidersColor(QColor color);
+  void changeSlidersColor(QColor color);
 
-    /**
+  /**
    * @brief four change the size of the canvas to four
    */
-    void four();
+  void four();
 
-    /**
+  /**
    * @brief eight change the size of the canvas to eight
    */
-    void eight();
-    /**
+  void eight();
+  /**
    * @brief sixteen change the size of the canvas to sixteen
    */
-    void sixteen();
+  void sixteen();
 
-    /**
+  /**
    * @brief thirtyTwo change the size of the canvas to thirtytwo
    */
-    void thirtyTwo();
+  void thirtyTwo();
 
-    /**
+  /**
    * @brief sixtyFour change the size of the canvas to sixtyfour
    */
-    void sixtyFour();
+  void sixtyFour();
 
-    /**
+  /**
    * @brief show boxes of available canvas sizes.
    */
-    void showBoxes();
+  void showBoxes();
 
-    /**
+  /**
    * @brief hide boxes of available canvas sizes.
    */
-    void hideBoxes();
+  void hideBoxes();
 
-    /**
-     * @brief onFPSValueChanged
-     */
-    void onFPSValueChanged();
+  /**
+   * @brief onFPSValueChanged
+   */
+  void onFPSValueChanged();
 
 signals:
-    /**
+  /**
    * @brief changeModelCurrentColor change the current selected color
    * @param red red value
    * @param green green value
    * @param blue blue value
    */
-    void changeModelCurrentColor(int red, int green, int blue);
+  void changeModelCurrentColor(int red, int green, int blue);
 
-    /**
+  /**
    * @brief changeTool change the current selected tool
    * @param toolName the tool to be changed to
    */
-    void changeTool(QString toolName);
+  void changeTool(QString toolName);
 
-    /**
-     * @brief save
-     */
-    void save();
+  /**
+   * @brief sends a save signal to the model
+   */
+  void save();
 
-    /**
-     * @brief load
-     */
-    void load();
+  /**
+   * @brief sends a load signal to the model
+   */
+  void load();
 
-    /**
+  /**
    * @brief getSize send the size of the new canvas
    * @param size the new size
    */
-    void getSize(QSize size);
+  void getSize(QSize size);
 
-    /**
-     * @brief changeFPS
-     * @param fps
-     */
-    void changeFPS(int fps);
+  /**
+   * @brief changeFPS
+   * @param fps
+   */
+  void changeFPS(int fps);
 
 private:
-    // The UI for this MainWindow
-    Ui::MainWindow *ui;
+  // The UI for this MainWindow
+  Ui::MainWindow *ui;
 
-    /**
+  /**
    * @brief loadStyleSheet set the StyleSheet
    */
-    void loadStyleSheet();
+  void loadStyleSheet();
 
-    /**
+  /**
    * @brief initCanvas set the drawing canvas
    */
-    void initCanvas();
+  void initCanvas();
 
-    /**
-   * @brief initPreviews set the preview canvas
+  /**
+   * @brief initPreviews set the preview animation playback frame
    */
-    void initPreviews();
+  void initPreviews();
 
-    /**
+  /**
    * @brief initSliders set the sliders
    */
-    void initSliders();
+  void initSliders();
+  /**
+   * @brief SetIcons sets the various icons in our buttons
+   */
+  void SetIcons();
+
+  /**
+   * @brief SetFileTab set the Save/Load buttons
+   */
+  void SetFileTab();
+
+  /**
+   * @brief initSizeButtons set the canvas resize buttons
+   */
+  void initSizeButtons();
 };
 #endif // MAINWINDOW_H
